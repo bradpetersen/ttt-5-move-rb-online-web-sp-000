9ba1f042fa07447d33b4f527268ff6c431118705
Brad Petersen <brad.petersen7@gmail.com>
2020-07-04 02:50:29 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -1,4 +1,8 @@
 # code your input_to_index and move method here!
+<<<<<<< HEAD
+=======
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+>>>>>>> cdd6715a1a637a8072816b2854dda2a5562027b2
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -11,6 +15,12 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end
 
+<<<<<<< HEAD
 def move(board, position, player = "X")
   board[position] = player
+=======
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+def move(board, index, player = "X")
+  board[index] = player
+>>>>>>> cdd6715a1a637a8072816b2854dda2a5562027b2
 end
